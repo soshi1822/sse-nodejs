763c8ad50f181458d0f1a739a421204f333c7af2
soshi1822 <35869573+soshi1822@users.noreply.github.com>
2019-12-28 22:19:02 +0900

    Add types file.

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+import { ServerResponse } from 'http' ;
+
+default class SSE {
+  constructor(res: ServerResponse, optional?: { statusCode?: number, headers?: { [key: string]: string } })
+
+  send(event: string, data: string | Object | Array<any>, id?: string | number, retry?: number): void
+  send(data: { id?: string | number, event?: string, retry?: number, data: string | Object | Array<any> }): void
+
+  keepAlive(): void
+
+  close(): void
+}
